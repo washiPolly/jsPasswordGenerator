f2e0a7fe5e71a9885daed1f86b3179e928ba255e
washiPolly <pollylsu@gmail.com>
2020-02-27 18:39:22 -0800

    worked more on js with tutor, added functions

--- script.js
+++ script.js
@@ -1,29 +1,80 @@
 //prompt user how many numbers, store in var
-// var pwLength = prompt("Please Enter your Password length, must be between 8-128");
+//praseInt converts String number to Integer
+var pwLength = parseInt(prompt("Please Enter your Password length, must be between 8-128"));
+
+if (pwLength < 8) {
+    alert('Password length must be at least 8 characters');
+    return;
+  }
+
 //prompt user lower? store in var
 var pwLower = prompt("Would you like to include Lower Cases? Y/N ");
 //prompt user upper? store in var
-// var pwUpper = prompt("Would you like to include Upper Cases? Y/N ");
+var pwUpper = prompt("Would you like to include Upper Cases? Y/N ");
 //prompt user number? store in var
-// var pwNumber = prompt("Would you like to include Numbers? Y/N ");
+var pwNumber = prompt("Would you like to include Numbers? Y/N ");
 //prompt user symbol? store in var
-// var pwSymbol = prompt("Would you like to include Symbols? Y/N ");
+var pwSymbol = prompt("Would you like to include Symbols? Y/N ");
 
-// console.log(pwLength, pwLower, pwUpper, pwNumber, pwSymbol);
+if (pwLower && pwUpper && pwNumber && pwSymbol === "n"){
+    alert("At Least one charager needs to be present");
+    
+}
 
+//disply pw document.textContent
+//addEventListener and identify element to display in text box
+console.log(pwLength, pwLower, pwUpper, pwNumber, pwSymbol);
+
+//create loops to validate Inputs (number between 8-128 and also if input is all "N")
 
 //check which options selected
 //generate function to get random passwords for each type
 function getRandomLower(){
     if (pwLower = "y"){
     return String.fromCharCode(Math.floor(Math.random() *26) + 97); 
-    
     }
 }
+
+function getRandomUpper(){
+    if (pwUpper = "y"){
+        return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+    }
+}
+
+function getRandomNum(){
+    if (pwNumber = "y"){
+        return (Math.floor(Math.random() *10));
+    }
+}
+
+function getRandomSym () {
+    if (pwSymbol = "y"){
+    // example set of special chars as a string in no particular order
+    var s = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    // generating a random index into the string and extracting the character at that position
+    return s.substr(Math.floor(s.length*Math.random()), 1);
+    } 
+}
 console.log(getRandomLower());
+console.log(getRandomUpper());
+console.log(getRandomNum());
+console.log(getRandomSym());
+
+    
 
 
 //loop to repeat for length, how to concat?
+//create random array
+function getRandomElement(arr){
+    var randomIndex = Math.floor(Math.random() * arr.pwLength);
+    var randomElement = arr[randomIndex];
+        return randomElement;
+        
+}
+
+console.log(getRandomElement);
+
+
 //generate event to listen for click
 //clipboard
 
